9c25d978b14aa5ff7d98cd91b0c69a778db2fd7f
Daniel Nutz <leinad.nutz@gmail.com>
2015-01-15 15:13:47 +0100

    FUNKTIONIERE

--- core/src/com/mygdx/codeAssets/Objects/Enity.java
+++ core/src/com/mygdx/codeAssets/Objects/Enity.java
@@ -1,6 +1,8 @@
 package com.mygdx.codeAssets.Objects;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
@@ -67,19 +69,46 @@ public abstract class Enity {
 	public Vector3 collisionDetection(Vector3 a_moveVector, GameMap a_map)
 	{
 		Vector3 collisionPoint = new Vector3(0,0,0);
+		Vector3 newPosition = new Vector3(position.add(a_moveVector));
+		//float moveLen = a_moveVector.len();
+		Vector3 calcFieldMin = new Vector3(0,0,0);
+		Vector3 calcFieldMax = new Vector3(0,0,0);
 		
-		Vector3 newPositionP1 = position.add(a_moveVector);
-		Vector3 newPositionP2 = new Vector3((newPositionP1.x + collisionRect.point2.x), 
-				(newPositionP1.y + collisionRect.point2.y), newPositionP1.z);
-		Vector3 newTilePositionP1 = Tile.convertWorldSpaceToTileSpace(newPositionP1);
-		Vector3 newTilePositionP2 = Tile.convertWorldSpaceToTileSpace(newPositionP2);
 		
-
-		Vector3 positionP2 = new Vector3(position.x + collisionRect.point2.x, position.y + collisionRect.point2.y, position.z);
+		calcFieldMin.x = Math.min(newPosition.x, Math.min(newPosition.x + collisionRect.point2.x, Math.min(position.x, position.x + collisionRect.point2.x)));
+		calcFieldMin.y = Math.min(newPosition.y, Math.min(newPosition.y + collisionRect.point2.y, Math.min(position.y, position.y + collisionRect.point2.y)));
+		calcFieldMin.z = Math.min(newPosition.z, position.z);
 		
-		tilePosition = Tile.convertWorldSpaceToTileSpace(position);
-		Vector3 tilePositionP2 = Tile.convertWorldSpaceToTileSpace(positionP2);
+		calcFieldMax.x = Math.max(newPosition.x, Math.max(newPosition.x + collisionRect.point2.x, Math.max(position.x, position.x + collisionRect.point2.x)));
+		calcFieldMax.y = Math.max(newPosition.y, Math.max(newPosition.y + collisionRect.point2.y, Math.max(position.y, position.y + collisionRect.point2.y)));
+		calcFieldMax.z = Math.max(newPosition.z, position.z);
 		
+		calcFieldMin = Tile.convertWorldSpaceToTileSpace(calcFieldMin);
+		calcFieldMax = Tile.convertWorldSpaceToTileSpace(calcFieldMax);
+		
+		Tile[][][] currentTileList = a_map.getTileSubsection(calcFieldMin, calcFieldMax);
+		
+		for (int dimX = 0; dimX < calcFieldMax.x - calcFieldMin.x; dimX++) {
+			for (int dimY = 0; dimY < calcFieldMax.y - calcFieldMin.y; dimY++) {
+				for (int dimZ = 0; dimZ < calcFieldMax.z - calcFieldMin.z; dimZ++) {
+					
+					
+					
+				}
+			
+			
+			}
+			
+			
+		}
+		
+		
+		if(newPosition.x < (newPosition.x + collisionRect.point2.x))
+		{
+			
+			
+		}
+			
 		
 		
 		
